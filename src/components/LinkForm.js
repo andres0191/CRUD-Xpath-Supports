ea0f97f00068b43fb619770b574ffe43738bea3a
andres0191 <andres_felipe0191@hotmail.com>
2021-06-30 22:52:57 -0500

    add function that capture data

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,21 +1,71 @@
+import { useEffect, useState } from "react";
 import React from 'react';
 
 const LinkForm = () => {
+
+    const initialStateValues = {
+        url: "",
+        name: "",
+        description: "",
+      };
+    
+    const [values, setValues] = useState(initialStateValues);
+    
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values)
+    }
+
+
     return (
-        <form className="card card-body">
+        <form className="card card-body" onSubmit={handleSubmit}>
             <div className="form-group input-group">
                 <div className="input-group-text bg-light">
-                <i class="material-icons">insert_link</i>
+                <i className="material-icons">insert_link</i>
                 </div>
                 <input
                     type="text"
                     className="form-control"
-                    placeholder="https://someurl.com"
+                    placeholder="https://someurl.xyz"
+                    value={values.url}
                     name="url"
-                    />
+                    onChange={handleInputChange}
+                />
             </div>
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                    <i className="material-icons">create</i>
+                </div>
+                <input
+                    type="text"
+                    value={values.name}
+                    name="name"
+                    placeholder="Website Name"
+                    className="form-control"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div className='form-group'>
+            <textarea
+                rows="3"
+                className="form-control"
+                placeholder="Write a Description"
+                name="description"
+                value={values.description}
+                onChange={handleInputChange}
+            ></textarea>
+            </div>
+            <button className="btn btn-primary btn-block">
+                Save
+            </button>
         </form>
-    )
-}
+    );
+};
 
-export default LinkForm
+export default LinkForm;
